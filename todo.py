f0eec5adf12fa2df553f5b77caf00dd7095d80ef
Nikola Stamatovic <stamatmail@gmail.com>
2015-11-01 17:41:26 +0100

    Added time tracking + field get and set methods

--- todo.py
+++ todo.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
-import sys, os, re, datetime, time
+'''
+    Simple CLI TODO program with time tracking
+    by Stamat <stamatmail@gmail.com>
+'''
+
+import sys, os, re, time
+from datetime import datetime, date, timedelta
+import threading
 import csv
 
 args = sys.argv
 args.pop(0)
 args = ' '.join(args)
 
-fieldnames = ['task', 'created', 'important', 'due', 'time_spent', 'tasklist', 'tags' ]
+fieldnames = ['task', 'created', 'important', 'due', 'time_spent', 'tasklist', 'tags', 'last_modified' ]
 filename = 'todo.csv'
 tmp_filename = 'tmp_todo.csv'
 filename_completed = 'todo_completed.csv'
@@ -17,25 +24,76 @@ pat_cmds = re.compile(r"(\-\-?[a-zA-Z0-9\-]+\s?[^\-]*)")
 pat_sepcmd = re.compile(r"(\-\-?)([a-zA-Z0-9\-]+)\s?([^\-]*)")
 m = pat_cmds.findall(args)
 
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec) 
+        func()  
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+#updated print
+def uprint(new):
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    print(CURSOR_UP_ONE + ERASE_LINE + str(new))
+
 def delete(num):
-    num = int(num)
     csv_in = open(filename)
     csv_out =  open(tmp_filename, 'w')
     reader = csv.DictReader(csv_in)
+    reader = list(reader)
+    
+    if num == 'last':
+        num = len(reader)
+    num = int(num)
+    
     writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
     writer.writeheader()
+    
     count = 1
     for row in reader:
         if count is not num:
             writer.writerow(row)
         count += 1
+        
     csv_in.close
     csv_out.close
     os.rename(tmp_filename, filename)
 
+def _set(num, field, value):
+    csv_in = open(filename)
+    csv_out =  open(tmp_filename, 'w')
+    reader = csv.DictReader(csv_in)
+    reader = list(reader)
+    
+    if num == 'last':
+        num = len(reader)
+    else: 
+        num = int(num)
+    num -= 1
+    
+    writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    try:
+        if field:
+            reader[num][field] = value
+        else:
+            reader[num] = value
+        reader[num]['last_modified'] = time.time()
+    except IndexError:
+        print 'Error: Nonexistent entry ' + str(num+1)
+    
+    for row in reader:
+        writer.writerow(row)
+        
+    csv_in.close
+    csv_out.close
+    os.rename(tmp_filename, filename)
 
-def list(args=None):
-    csv_in = open('todo.csv')
+def display(args=None):
+    csv_in = open(filename)
     reader = csv.DictReader(csv_in)
     count = 1
     for row in reader:
@@ -43,11 +101,57 @@ def list(args=None):
         count += 1
     csv_in.close
 
+def _get(num, field='asd'):
+    csv_in = open(filename)
+    reader = csv.DictReader(csv_in)
+    reader = list(reader)
+    
+    if num == 'last':
+        num = len(reader)
+    else: 
+        num = int(num)
+    num -= 1
+    
+    try:
+        if field:
+            return reader[num][field]
+        else:
+            return reader[num]
+    except IndexError:
+        print 'Error: Nonexistent entry ' + str(num+1)
+        
+    csv_in.close
+
 def complete(arg):
     pass
 
-def track(): #task time tracking
-    pass
+def track(num): #task time tracking
+    #get the previous value, if empty start tiem is time now
+    st = _get(num, 'time_spent')
+    if st is '':
+        st = time.time()
+    else:
+        st = time.time()-float(st)
+        
+    def timed_output(st, delay):
+        while True:
+            d = timedelta(seconds=time.time() - st)
+            uprint(d)
+            time.sleep(delay)
+            
+    delay = 1
+    t = threading.Thread(target = timed_output, args = (st,delay))
+    
+    print
+    t.daemon = True
+    t.start()
+    
+    try:
+        while True:
+            time.sleep(delay)
+    except KeyboardInterrupt:
+        st = time.time() - st
+        _set(num, 'time_spent', st)
 
 def add_time(): #add hours to hours spent
     pass
@@ -58,18 +162,26 @@ def due(): #set due of the task, keywords like today, tomorrow, day after tomorr
 def important(): #set the importance flag
     pass
 
-def tasklist(): #asigns a task to a task list / project
+def tasklist(args): #asigns a task to a task list / project
+    
     pass
 
 def tags(): #asigns tags to he task, add tags to a task list, remove the tags, etc..
     pass
+
+def imprt(): #imports a CSV
+    pass
+
 
 def new(args):
     if not os.path.exists(filename):
         csv_out =  open(filename, 'w')
         writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerow({'task': args, 'created': time.time()})
+        writer.writerow({'task': args,
+                         'created': time.time(),
+                         'important': 0,
+                         'due': 'someday'})
         csv_out.close
     else:
         csv_in = open(filename)
@@ -79,7 +191,10 @@ def new(args):
         writer.writeheader()
         for row in reader:
             writer.writerow(row)
-        writer.writerow({'task': args, 'created': time.time()})
+        writer.writerow({'task': args,
+                         'created': time.time(),
+                         'important': 0,
+                         'due': 'someday'})
         csv_in.close
         csv_out.close
         os.rename(tmp_filename, filename)
@@ -91,8 +206,10 @@ fn = {
     'delete': delete,
     'c': complete,
     'complete': complete,
-    'l': list,
-    'list': list
+    'l': display,
+    'list': display,
+    't': track,
+    'track': track
 }
 
 def execute(command, args=None):
@@ -120,7 +237,7 @@ if m:
 else:
     #add new todo
     if args.strip() == '':
-       list()
+       display()
     else:
         print args
         new(args)
